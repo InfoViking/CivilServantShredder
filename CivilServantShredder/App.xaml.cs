b275af1cf264f12e404ec9d90a33b80b7f13033e
Mrochen, David <david.mrochen@mediservbank.de>
2024-11-09 10:29:15 +0100

    Initial Commit

---
+++ CivilServantShredder/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CivilServantShredder
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
