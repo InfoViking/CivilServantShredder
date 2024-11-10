b782306a047cc40c67277a5e38ad72b5eac2c8e8
Feick, Daniel <daniel.feick@mediservbank.de>
2024-11-10 11:30:57 +0100

    Create Post eingebaut und Text Only Erstellungsmaske

--- Adminbereich.View/Views/New_BlogPost_TextOnly.xaml.cs
+++ Adminbereich.View/Views/New_BlogPost_TextOnly.xaml.cs
@@ -7,4 +7,42 @@ public partial class New_BlogPost_TextOnly : ContentPage
 		InitializeComponent();
 	}
 
+
+
+    private void Button_Post_Clicked(object sender, EventArgs e)
+    {
+        Post();
+    }
+
+    private async void Post()
+    {
+        bool shouldPost = await DisplayAlert("Post?", "Are you sure you want to Post now?", "yes", "no");
+
+        if (shouldPost)
+        {
+            createAndSendPostToApi();
+        }
+        else
+        {
+            await close();
+        }
+    }
+
+    private async Task close()
+    {
+        bool shouldDiscard = await DisplayAlert("Discard?", "Do you want to Discard your Post?", "yes", "no");
+
+        if (shouldDiscard) { await Navigation.PopAsync(); }
+    }
+
+    private async void createAndSendPostToApi()
+    {
+        await DisplayAlert("Chill mo!", "Do gehts noch net weida", "ok");
+        await close();
+    }
+
+    private async void Button_Cancle_Clicked(object sender, EventArgs e)
+    {
+        await close();
+    }
 }
