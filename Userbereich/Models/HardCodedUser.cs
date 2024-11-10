e0ebb7648c66b9671856115f1912c84195d614c0
Toussaint <dtt@firma.mediserv.de>
2024-11-10 13:55:45 +0100

    add hardcoded User and Admin in models

---
+++ Userbereich/Models/HardCodedUser.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Userbereich.Models
+{
+    static class HardCodedUser
+    {
+        static Guid Id = Guid.NewGuid();
+        static string FirstName = "Herbert";
+        static string LastName = "Schmidt";
+        static string Street = "Hauptstrasse.12";
+        static string PostalCode = "12345";
+        static string Locality = "Hauptstadt";
+        static Guid CommunityId = Guid.NewGuid();
+        static Role UserRole = Role.User;
+
+        [EmailAddress]
+        static string Email = "herbert.schmidt@example.de";
+    }
+}
